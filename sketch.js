e7568e6d6cd686693333c6a081c4706bf6bd0d46
snehatawade <54199703+snehatawade@users.noreply.github.com>
2019-11-08 19:48:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
-var canvas, bg;
+var canvas, backgroundImage;
+
 var gameState = 0;
+var playerCount;
+
 var database;
-var playerCount = 0;
+
 var form, player, game;
+var allPlayers;
+var distance = 0;
 
 function setup(){
   canvas = createCanvas(400,400);
@@ -13,59 +18,11 @@ function setup(){
 }
 
 function draw(){
-  
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState = 1){
+    clear();
+    game.play();
+  }
 }
-
-/*var hypnoticBall, database;
-var position;
-
-
-function setup(){
-  database = firebase.database();
-  console.log(database);
-  createCanvas(500,500);
-
-  hypnoticBall = createSprite(250,250,10,10);
-  hypnoticBall.shapeColor = "red";
-
-
-  var hypnoticBallPosition = database.ref('ball/position');
-  hypnoticBallPosition.on("value", readPosition, showError);
-}
-
-function draw(){
-  background("white");
-  
-    if(keyDown(LEFT_ARROW)){
-      writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-      writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-      writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-      writePosition(0,+1);
-    }
-    drawSprites();
-  
-}
-
-function writePosition(x,y){
-  database.ref('ball/position').set({
-    'x': position.x + x ,
-    'y': position.y + y
-  })
-}
-
-function readPosition(data){
-  position = data.val();
-  console.log(position.x);
-  hypnoticBall.x = position.x;
-  hypnoticBall.y = position.y;
-}
-
-function showError(){
-  console.log("Error in writing to the database");
-}*/
